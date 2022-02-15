743f830e4e1b1e030be4f00e638f94ec3522283d
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-15 10:03:02 +0100

    added functional, still not functional

--- src/components/Grades.vue
+++ src/components/Grades.vue
@@ -5,8 +5,8 @@
     </v-row>
     <v-row>
       <h3>Introduce your mark, and I'll tell you your grade.</h3>
-      <input type="text" id="input-txt" />
-      <button>Send mark</button>
+      <input v-model="" type="text" id="input-txt" />
+      <button v-on:click="grades()">Send mark</button>
     </v-row>
   </v-container>
 </template>
@@ -16,5 +16,21 @@ export default {
   name: "Grades",
 
   data: () => ({}),
+  methods: {
+    grades(num) {
+      if (num >= 0 && num <= 10) {
+        return num >= 9
+          ? "Outstanding"
+          : num < 9 && num >= 7
+          ? "Remarkable"
+          : num < 7 && num >= 5
+          ? "Suficient"
+          : num < 5 && num >= 3
+          ? "Insuficient"
+          : "Too poor";
+      }
+      return "Enter a number between 0 and 10";
+    },
+  },
 };
 </script>
